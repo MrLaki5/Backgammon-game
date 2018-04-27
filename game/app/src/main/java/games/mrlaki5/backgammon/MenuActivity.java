1fa444d6fa4b1fc69e45fdd6536bb92b9bbfad69
MrLaki5 <mimar1207@gmail.com>
2018-04-28 00:24:15 +0200

    menu added, game activity with backgroud, dice images

--- game/app/src/main/java/games/mrlaki5/backgammon/MenuActivity.java
+++ game/app/src/main/java/games/mrlaki5/backgammon/MenuActivity.java
@@ -1,7 +1,9 @@
 package games.mrlaki5.backgammon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void startNewGame(View view) {
+        Intent intent= new Intent(MenuActivity.this, GameActivity.class);
+        startActivity(intent);
     }
 }
